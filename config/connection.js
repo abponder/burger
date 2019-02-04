088f6ae760548c1becd79425311bc53c2565a7de
abponder <32970225+abponder@users.noreply.github.com>
2019-02-04 11:33:15 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL){
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Blue*redx11963",
+    password: "xxxxxxxxxx",
     database: "burger_db"
 });
 }
